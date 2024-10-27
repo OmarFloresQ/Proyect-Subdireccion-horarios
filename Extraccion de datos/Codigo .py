1ad9bc2e6fc4c61f43644400b5f7fb0402092173
OmarFloresQ <167242893+OmarFloresQ@users.noreply.github.com>
2024-10-27 02:52:08 -0700

    Actualización para tablas SQL .py

--- Extraccion de datos/Codigo .py
+++ Extraccion de datos/Codigo .py
@@ -1,7 +1,6 @@
 import openpyxl
 import pandas as pd
-from tabulate import tabulate 
-
+from tabulate import tabulate
 
 # Cargar el archivo de Excel
 excel_dataframe = openpyxl.load_workbook("Copia de Ejemplo Disponibilidad.xlsx")
@@ -20,8 +19,39 @@ colores_disponibilidad = {
     "FFFFE599": "disponible"           # Amarillo (#ffe599)
 }
 
+# Diccionario para mapear cada horario a un ID secuencial
+horario_ids = {
+    "7:00-8:00": 1,
+    "8:00-9:00": 2,
+    "9:00-10:00": 3,
+    "10:00-11:00": 4,
+    "11:00-12:00": 5,
+    "12:00-13:00": 6,
+    "13:00-14:00": 7,
+    "14:00-15:00": 8,
+    "15:00-16:00": 9,
+    "16:00-17:00": 10,
+    "17:00-18:00": 11,
+    "18:00-19:00": 12,
+    "19:00-20:00": 13,
+    "20:00-21:00": 14,
+    "21:00-22:00": 15
+}
+
+#Diccionario para los dias de la semana, convertir a ID
+dias_ids = {
+    "LUNES": 1,
+    "MARTES": 2,
+    "MIÉRCOLES" or "MIERCOLES": 3,
+    "JUEVES": 4,
+    "VIERNES": 5,
+    "SÁBADO (virtual)": 6,
+    "DOMINGO": 7
+}
+
 # Inicializar lista para almacenar todos los datos
 todos_datos = []
+Datos = []
 
 # Iterar sobre cada hoja de trabajo, comenzando desde la segunda hoja
 for nombre_hoja in excel_dataframe.sheetnames[0:]:
@@ -38,9 +68,11 @@ for nombre_hoja in excel_dataframe.sheetnames[0:]:
     # Crear registros para cada combinación de día y horario en la hoja actual
     for i, row in enumerate(range(fila_inicio_datos, fila_fin_datos + 1)):
         horario = horarios[i]  # Obtener el horario actual
+        id_horario = horario_ids.get(horario, "No definido")  # Obtener el ID del horario
 
         for j, col in enumerate(range(columna_inicio_disponibilidad, columna_fin_disponibilidad + 1)):
             dia = dias[j]  # Obtener el día correspondiente
+            id_dias = dias_ids.get(dia, "No definido")  # Obtener el ID del horario
             # Obtener la celda actual
             celda = hoja.cell(row=row, column=col)
 
@@ -60,14 +92,22 @@ for nombre_hoja in excel_dataframe.sheetnames[0:]:
             #if disponibilidad in ["disponible", "poca disponibilidad"]: 
                 #todos_datos.append([id_empleado, dia, horario, disponibilidad])
             
-            todos_datos.append([id_empleado, dia, horario, disponibilidad])
+             # Agregar solo los registros con disponibilidad "disponible" o "poca disponibilidad"
+            if disponibilidad in ["disponible", "poca disponibilidad"]:
+                # Asigno ID basado en la disponibilidad
+                 if disponibilidad == "disponible":
+                     id_disponibilidad = 1
+                 elif disponibilidad == "poca disponibilidad":
+                     id_disponibilidad = 2
+                 todos_datos.append([id_empleado, id_dias, id_horario, id_disponibilidad])
+                 Datos.append([id_empleado, dia, horario, disponibilidad])
 
-# Guardar todos los datos en un único archivo CSV llamado 'disponibilidades.csv'
-df = pd.DataFrame(todos_datos, columns=["ID_Empleado", "Día", "Horario", "Disponibilidad"])
-df.to_csv("disponibilidades.csv", index=False, encoding="utf-8")
+df = pd.DataFrame(todos_datos, columns=["ID_Empleado", "ID_Dias", "ID_Horario", "ID_Disponibilidad"])
+df.to_csv("disponibilidades_ids.csv", index=False, encoding="utf-8")
+print(tabulate(df, headers="keys", tablefmt="fancy_grid"))
+print("Todos los datos con ids han sido guardados en 'disponibilidades_ids.csv'.")
 
-
-#por si quiere visualizar la tabla de los datos recabados
-#print(tabulate(df, headers="keys", tablefmt="fancy_grid")) 
-
-print("Todos los datos han sido guardados en 'disponibilidades.csv'.")
+#df2 = pd.DataFrame(Datos, columns=["ID_Empleado", "Dia", "Horario", "Disponibilidad"])
+#df2.to_csv("disponibilidades.csv", index=False, encoding="utf-8")
+#print(tabulate(df2, headers="keys", tablefmt="fancy_grid"))
+#print("Todos los datos completos han sido guardados en 'disponibilidades.csv'.")
